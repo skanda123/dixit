94a69a862ad15d6c7ece27d3f1181953a42f1e49
skanda123 <35725149+skanda123@users.noreply.github.com>
2018-01-24 12:26:23 +0530

    Add files via upload

---
+++ neg.py
@@ -0,0 +1,6 @@
+#program to check number is postive or negative
+a=int(input("enter the number is postive or negative"))
+if a>0:
+   print("given number is postive")
+else:
+    print("given number is negative")
