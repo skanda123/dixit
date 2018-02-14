d2c7215e813582d349dfb9f3759de99c6a53c7d2
skanda123 <35725149+skanda123@users.noreply.github.com>
2018-02-14 11:28:11 +0530

    Delete ail.py

--- ail.py
+++
@@ -1,8 +0,0 @@
-#sending a email to a friend
-import smtplib
-server = smtplib.SMTP('smtp.gmail.com',587)
-server.starttls()
-server.login("dixitshetty930@gmail.com", "**********")
-msg = "HI"
-server.sendmail("dixitshetty930@gmail.com","vikramgowda02@gmail.com",msg)
-server.quit()
